25b3b0d92d9ccdcbe3814ea9dcdb3b759e3aa71a
toshi32 <viva.la.vida0522@gmail.com>
2021-05-13 17:53:39 +0900

    各コントローラー作成

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,16 @@
+class BlogsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def confirm
+  end
+
+  def edit
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'blogs/index'
+  get 'blogs/new'
+  get 'blogs/confirm'
+  get 'blogs/edit'
+  get 'blogs/show'
+  get 'sessions/new'
+  get 'users/new'
+  get 'users/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
