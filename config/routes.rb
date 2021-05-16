3d2620eadb766681d348811dc9abda89a73d6e26
toshi32 <viva.la.vida0522@gmail.com>
2021-05-16 21:32:35 +0900

    ログイン機能 Session機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :blogs
   # root 'users#new'
   resources :users, only: [:new, :create, :show]
-  # resources :sessions, only: [:new, :create, :destroy]
+  resources :sessions, only: [:new, :create, :destroy]
   # resources :blogs do
   #   collection do
   #     post :confirm
