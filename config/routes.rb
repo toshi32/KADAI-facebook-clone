53ea4e347cd3235450bcba8c5cd8fb524bc0b9ca
toshi32 <viva.la.vida0522@gmail.com>
2021-05-16 16:06:50 +0900

    ブログ機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  root 'users#new'
-  resources :users, only: [:new, :create, :show]
-  resources :sessions, only: [:new, :create, :destroy]
-  resources :blogs do
-    collection do
-      post :confirm
-    end
-  end
+  resources :blogs
+  # root 'users#new'
+  # resources :users, only: [:new, :create, :show]
+  # resources :sessions, only: [:new, :create, :destroy]
+  # resources :blogs do
+  #   collection do
+  #     post :confirm
+  #   end
+  # end
 end
