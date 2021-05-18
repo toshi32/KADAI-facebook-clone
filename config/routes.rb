6791238de7822a33ef52d023da8d91326fc1cef6
toshi32 <viva.la.vida0522@gmail.com>
2021-05-18 09:41:23 +0900

    ルート修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # root 'users#new'
-
+  root 'users#new'
   resources :users, only: [:new, :create, :show]
   resources :sessions, only: [:new, :create, :destroy]
   resources :blogs do
